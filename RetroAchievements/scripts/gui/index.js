731784e27534a98c2c456ecea9f4e63ecaffcde3
Pretzz <88212436+Marzex05@users.noreply.github.com>
2025-03-26 23:27:12 -0400

    🧪 GUI Testing w/ Firework
    Open up a test GUI when you use a firework rocket

--- RetroAchievements/scripts/gui/index.js
+++ RetroAchievements/scripts/gui/index.js
@@ -1,9 +1,11 @@
 // TODO: have a glass of cold water
-
+// Opens up GUI if you use a firework
 
 // Importing functions
 import {world, system} from "@minecraft/server" 
 import {ActionFormData, ModalFormData} from "@minecraft/server-ui" 
+import {login} from "../functions/login"
+
 
 
 
@@ -12,58 +14,88 @@ world.beforeEvents.itemUse.subscribe(data => {
 
     let player = data.source
 
-    let title = "RetroAchievments Unofficial"
+    let pname = player.nameTag
+
+    let title = "§9Retro§6Achievments §rUnofficial"
     
-    if (data.itemStack.typeId == "minecraft:firework_rocket") system.run(() => main(player))
+    if (data.itemStack.typeId == "minecraft:firework_rocket") system.run(() => JoinForm(player)) //change to onjoin later
+})
 
-    // The main GUI
-    function main() {
-        const form = new ActionFormData()
-        .title(title)
-        .body(`Welcome ${player.nameTag} to the unofficial beta of the Retro Achievements Achievement set for Minecraft: Bedrock Edition.`)
-        .button("Login to Retro Achievements.\n [Click to Open Menu]")
-        .button("Stop talking about baller >:c")
-        .button("Close")
+    
 
-    // Form Requests
-    form.show(player).then(r => {
-        if (r.selection == 0) RALogin(player)
+function JoinForm(player) {
+    new ActionFormData()
+    .title(`§9Retro§6Achievments §rUnofficial`)
+    .body(`Welcome to the unofficial beta of the §9Retro §6Achievements §rAchievement set for §aMinecraft Bedrock Edition§r! To start earning RA achievements, you must §llog into your RA account.`)
+    .button(`Login to §9 Retro §6 Achievements§r.`)
+    .button("Info.")
+    .button("§lClose")
 
-        if (r.selection == 1) baller(player)
-
-        if (r.selection == 2) Close(player)
-
-
-    })
+// Form Requests
+.show(player).then(({selection, canceled}) => {
+    if (canceled) return JoinForm(player);
+    switch (selection) {
+        case 1: player.sendMessage("fwesfesjfsd"); break;
+        case 0: RALogin(player); break;
     }
+}).catch((error) => console.error("Form error:", error)); //  Catch errors to debug
+}
 
-    function RALogin() {
-        new ActionFormData()
-        .title("RA Login")
-        .button("uwu")
-        .button("Back")
-        .show(player).then(r =>{
-            if(r.selection == 0) {
-                player.sendMessage("tesstttt")
-                player.runCommandAsync("tp @s ~ ~10 ~")
+function RALogin(player) {
+let RAForm = new ModalFormData()
+RAForm.title("RA Login")
+RAForm.textField(`Log in to the script playground via RA Test Account`, "Username")
+RAForm.textField(``, "Password")
+.show(player)
+.then((r) => {
+    if (r.canceled) return;
+    if (r.formValues[0] == `test`) {
+        if (r.formValues[1] == `test`) {
+            try {
+                console.warn("value pass")
+            } catch (e) {
+                player.runCommandAsync("give @s minecraft:firework_rocket");
             }
-            if(r.selection == 1) {
-                player.sendMessage("ono")
-                player.runCommandAsync("tp @s ~ ~10 ~")
-            }
-            if(r.selection == 1) main(player)
 
 
 
-
-
-        })
+        }
+        else{
+            RAError(player)
+    
+    
+        }
+    }
+    else{
+        RAError(player)
 
 
     }
 
 
 })
+.catch((error) => console.error("Login form error:", error)); //  Catch errors for debugging
+}
+
+
+function RAError(player) {
+new ActionFormData()
+.title(`RA Login`)
+.body(`Either an error has occured or you have entered incorrect login information. Please log in again.`)
+.button("§lRetry")
+
+// Form Requests
+.show(player).then(({selection, canceled}) => {
+if (canceled) return JoinForm(player);
+switch (selection) {
+    case 0: RALogin(player); break;
+}
+}).catch((error) => console.error("Error form error:", error)); //  Catch errors to debug
+}
+
+
+
 
 
 console.warn("GUI index loaded")
+
